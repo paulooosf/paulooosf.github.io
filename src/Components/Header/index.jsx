91c86de622b8f3fe7ae48a77fc380d5312efc4b6
paulooosf <phpessoal2021@gmail.com>
2024-08-12 23:38:35 -0300

    feat: atualizando links

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -4,14 +4,14 @@ import logo from '../../assets/logo.png';
 
 export default function Header() {
   return (
-    <header className="container_header">
+    <header className="container_header" id='inicio'>
         <img src={logo} alt="Logotipo do desenvolvedor" className='header_logo'/>
         <ul className='header_lista'>
-          <li className="header_lista_item"><a href="" className="header_lista_item_link">Início</a></li>
-          <li className="header_lista_item"><a href="" className="header_lista_item_link">Sobre</a></li>
-          <li className="header_lista_item"><a href="" className="header_lista_item_link">Tecnologias</a></li>
-          <li className="header_lista_item"><a href="" className="header_lista_item_link">Projetos</a></li>
-          <li className="header_lista_item"><a href="" className="header_lista_item_link_contato">Contato</a></li>
+          <li className="header_lista_item"><a href="#inicio" className="header_lista_item_link">Início</a></li>
+          <li className="header_lista_item"><a href="#sobre" className="header_lista_item_link">Sobre</a></li>
+          <li className="header_lista_item"><a href="#tecnologias" className="header_lista_item_link">Tecnologias</a></li>
+          <li className="header_lista_item"><a href="#projetos" className="header_lista_item_link">Projetos</a></li>
+          <li className="header_lista_item"><a href="#contato" className="header_lista_item_link_contato">Contato</a></li>
         </ul>
     </header>
   )
